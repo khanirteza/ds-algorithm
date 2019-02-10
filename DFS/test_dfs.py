3ebd9479ede6542bf5acef51c19dbeabf54c15c7
Mohammad Irteza Khan <khanirteza@users.noreply.github.com>
2019-02-09 22:58:56 -0800

    Add extra test case in DFS

--- DFS/test_dfs.py
+++ DFS/test_dfs.py
@@ -12,8 +12,12 @@ g.add_edge(3, 4)
 g.add_edge(4, 5)
 
 
-def test_dfs():
+def test_dfs1():
     dfs = DFS()
     dfs.traverse(g.g, 1)
     assert dfs.early_vertex == [1, 2, 3, 4, 5, 6]
     
+def test_dfs2():
+    dfs = DFS()
+    dfs.traverse(g.g, 2)
+    assert dfs.early_vertex == [2, 1, 5, 4, 3, 6]
